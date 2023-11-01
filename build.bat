a20b71daf9d89448708385fde19938e025b299bf
chikuwa_empire <124578804+chikuwa-empire@users.noreply.github.com>
2023-11-02 01:19:04 +0900

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-zcc +msx -subtype=msxdos -DAMALLOC msx0.c envsampl.c -o envsampl.com -lm
-zcc +msx -subtype=msxdos -DAMALLOC msx0.c msx0lcd.c lcdsampl.c -o lcdsampl.com
+zcc +msx -subtype=msxdos -create-app -DAMALLOC msx0.c msx0env.c envsampl.c -o envsampl.com -lm
+zcc +msx -subtype=msxdos -create-app -DAMALLOC msx0.c msx0lcd.c lcdsampl.c -o lcdsampl.com
+zcc +msx -subtype=msxdos -create-app -DAMALLOC msx0.c msx0lcd.c lcdclock.c -o lcdclock.com
